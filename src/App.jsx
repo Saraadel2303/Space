0d89b9c45aa89a7d18e24ffddd7e252654942299
Saraadel2303 <Saraadelfarid990@gmail.com>
2025-04-17 02:07:38 +0200

    Initialize project with React and React Router setup

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import './App.css'
+import Home from './pages/Home/Home'
+import Destination from './pages/Destination/Destination'
+import Crew from './pages/Crew/Crew'
+import Technology from './pages/Technology/Technology'
+import Layout from './pages/Layout/Layout'
+
+
+const router = createBrowserRouter([
+  {
+    path:"" , element:<Layout/> , children:[{
+      index:true , element:<Home/>
+    } , {path:"/destenation" , element:<Destination/>} , {path:"/crew" , element:<Crew/>} , {path:"/technology" , element:<Technology/>}]
+  }
+])
+
+function App() {
+  
+  return (
+    <>
+      <RouterProvider router={router}></RouterProvider> 
+    </>
+  )
+}
+
+export default App
