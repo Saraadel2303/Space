9a091d1adf543662e1b1c05dfc1a555103210722
Saraadel2303 <Saraadelfarid990@gmail.com>
2025-04-25 14:30:29 +0300

    modify image in destination page

--- src/Data/Destinations.js
+++ src/Data/Destinations.js
@@ -2,7 +2,7 @@ const destinations = [
     {
       id: 0,
       name: "Moon",
-      image: "public/destination/image-moon.webp",
+      image: "/destination/image-moon.webp",
       description: "See our planet as you’ve never seen it before. A perfect relaxing trip away to help regain perspective and come back refreshed. While you're there, take in some history by visiting the Luna 2 and Apollo 11 landing sites.",
       distance: "384,400 km",
       travel: "3 days",
